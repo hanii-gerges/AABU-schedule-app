8ca82e93ccb06471c841be3a6af52742c23b5f7d
Hani Gerges <abualhen@gmail.com>
2020-03-29 00:16:11 +0300

    database migration added

---
+++ database/migrations/2020_03_03_205511_create_sections_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->string('id');
+            $table->string('name');
+            $table->integer('section');
+            $table->integer('hours');
+            $table->string('time_days');
+            $table->string('instructor');
+            $table->string('room');
+            $table->index('id');
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+    }
+}
